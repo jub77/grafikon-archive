ca41c54d9eb2bffc429d33d4bce89654a5158422
cz2b10k5 <cz2b10k5@cz2cbfdc>
2008-06-05 19:11:04 +0200

    import

---
+++ grafikon-templates/src/net/parostroj/timetable/model/templates/TemplateList.java
@@ -0,0 +1,26 @@
+package net.parostroj.timetable.model.templates;
+
+import java.util.Collections;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * List of templates. Loaded by JAXB.
+ * 
+ * @author jub
+ */
+@XmlRootElement(name = "template_list")
+public class TemplateList {
+
+    private List<Template> templates;
+    
+    @XmlElement(name = "template")
+    public List<Template> getTemplates() {
+        return templates;
+    }
+
+    public void setTemplates(List<Template> templates) {
+        this.templates = templates;
+    }
+}
