e19b74f295fd58218d9e02eedbf64d817d244095
jub <jub@parostroj.net>
2008-09-22 14:32:27 +0200

    + [gui] added logging into a file

--- grafikon-gui/src/net/parostroj/timetable/gui/Main.java
+++ grafikon-gui/src/net/parostroj/timetable/gui/Main.java
@@ -1,7 +1,11 @@
 package net.parostroj.timetable.gui;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import javax.swing.UIManager;
 
 /**
@@ -13,6 +17,15 @@ public class Main {
     static {
         Logger.getLogger("net.parostroj").setLevel(Level.FINE);
         Logger.getLogger("").getHandlers()[0].setLevel(Level.ALL);
+        
+        // add file output to logging
+        try {
+            Handler handler = new FileHandler("grafikon.log");
+            handler.setFormatter(new SimpleFormatter());
+            Logger.getLogger("").addHandler(handler);
+        } catch (IOException e) {
+            Logger.getLogger("net.parostroj").log(Level.WARNING, "Cannot initialize logging file.", e);
+        }
     }
 
     public static void main(String[] args) throws Exception {
