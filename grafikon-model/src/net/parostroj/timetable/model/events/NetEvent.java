52086ff43ec44bab960bf2d6c424d9d2a1af891a
jub <jub@parostroj.net>
2010-01-19 20:02:10 +0100

    + [model] net event extended with line, line class and node

--- grafikon-model/src/net/parostroj/timetable/model/events/NetEvent.java
+++ grafikon-model/src/net/parostroj/timetable/model/events/NetEvent.java
@@ -1,6 +1,9 @@
 package net.parostroj.timetable.model.events;
 
+import net.parostroj.timetable.model.Line;
+import net.parostroj.timetable.model.LineClass;
 import net.parostroj.timetable.model.Net;
+import net.parostroj.timetable.model.Node;
 import net.parostroj.timetable.visitors.EventVisitor;
 
 /**
@@ -13,13 +16,30 @@ public class NetEvent extends GTEvent<Net> {
     public enum Type {
         NODE_ADDED, NODE_REMOVED, LINE_ADDED, LINE_REMOVED, LINE_CLASS_ADDED, LINE_CLASS_REMOVED, NESTED
     }
-    private final Type type;
 
-    public NetEvent(Net net, Type type) {
+    private final Type type;
+    private Node node;
+    private Line line;
+    private LineClass lineClass;
+
+    public NetEvent(Net net, Type type, Node node) {
         super(net);
         this.type = type;
+        this.node = node;
     }
-    
+
+    public NetEvent(Net net, Type type, Line line) {
+        super(net);
+        this.type = type;
+        this.line = line;
+    }
+
+    public NetEvent(Net net, Type type, LineClass lineClass) {
+        super(net);
+        this.type = type;
+        this.lineClass = lineClass;
+    }
+
     public NetEvent(Net net, GTEvent<?> event) {
         super(net, event);
         this.type = Type.NESTED;
@@ -27,6 +47,18 @@ public class NetEvent extends GTEvent<Net> {
 
     public Type getType() {
         return type;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public LineClass getLineClass() {
+        return lineClass;
+    }
+
+    public Node getNode() {
+        return node;
     }
 
     @Override
